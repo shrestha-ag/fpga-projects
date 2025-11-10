6e2077ce6c7a7b38c66daa48acc11f1d3f9283dd
Shrestha Agarwal <shrestha.agarwal2000@gmail.com>
2025-11-09 20:09:54 -0700

    Marquee pattern for LEDs

--- pmod-leds-shr/pmod-led.v
+++ pmod-leds-shr/pmod-led.v
@@ -1,37 +1,62 @@
 // Plug PMOD LED on PMOD3 (Full Scale PMOD)
 
-module pmodled(input CLK, output LED, output PMOD1_D4, output PMOD2_D0, output PMOD3_D5, output PMOD4_D1, output PMOD5_D6, output PMOD6_D2, output PMOD7_D7, output PMOD8_D3);
-    reg [30:0] counter;
-    wire lclk = counter[14];
-
-    initial begin
-        PMOD1_D4 =  1'b0;
-        PMOD2_D0 =  1'b0;
-        PMOD3_D5 =  1'b0;
-        PMOD4_D1 =  1'b0;
-        PMOD5_D6 =  1'b0;
-        PMOD6_D2 =  1'b0;
-        PMOD7_D7 =  1'b0;
-        PMOD8_D3 =  1'b0;
-        LED      =  1'b0;
-        counter  = 31'h7FFFFFFF;
-    end
-
-    always @(posedge CLK)
+module pmodled(input CLK, output LED, PMOD1_D4, PMOD2_D0, PMOD3_D5, PMOD4_D1, PMOD5_D6, PMOD6_D2, PMOD7_D7, PMOD8_D3);
+	reg [31:0] counter;
+	reg [7:0] leds;
+	reg [7:0] leds_next;
+	reg [0:0] direction;
+	
+	assign PMOD7_D7 = !leds[7];
+	assign PMOD5_D6 = !leds[6];
+	assign PMOD3_D5 = !leds[5];
+	assign PMOD1_D4 = !leds[4];
+	assign PMOD8_D3 = !leds[3];
+	assign PMOD6_D2 = !leds[2];
+	assign PMOD4_D1 = !leds[1];
+	assign PMOD2_D0 = !leds[0];
+	assign LED  = !counter[23];
+	
+	initial 
     begin
-        counter <= counter - 1'b1;
-    end
+		counter = 0;
+		leds = 1;
+		direction = 1;
+		leds_next = 1;
+	end
 
-    always @(posedge lclk)
+	always @ (posedge CLK) 
     begin
-        PMOD1_D4 <= counter[26];
-        PMOD2_D0 <= counter[22];
-        PMOD3_D5 <= counter[27];
-        PMOD4_D1 <= counter[23];
-        PMOD5_D6 <= counter[28];
-        PMOD6_D2 <= counter[24];
-        PMOD7_D7 <= counter[29];
-        PMOD8_D3 <= counter[25];
-        LED      <= !LED;
-    end
+		counter <= counter + 1;
+		if (counter[10] && !counter[19]) 
+        begin
+			leds <= leds_next;
+		end
+		if (counter[19]) 
+        begin
+			if( direction ) 
+            begin
+				if (leds == 'b10000000 ) 
+                begin
+					leds_next <= leds >> 1;
+					direction <= 0;
+				end 
+                else 
+                begin
+					leds_next <= leds << 1;		
+				end
+			end 
+            else 
+            begin
+				if (leds == 'b00000001 )
+                begin
+					leds_next <= leds << 1;
+					direction <= 1;
+				end 
+                else 
+                begin
+					leds_next <= leds >> 1;		
+				end
+			end
+		end
+	end
 endmodule
